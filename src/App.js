0392250a5d81570617f785591b5128f319866027
Olha Mardar <omskazka@gmail.com>
2020-02-18 11:04:36 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './shell/header/header.component.jsx';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/shop' component={ShopPage} />
         <Route path='/' component={HomePage} />
